e5503d2ea951f7f06c96fb22c4804351e867f9de
uzziah lukeka <uzziahlukeka@gmail.com>
2024-08-06 15:49:07 +0200

    contact update

--- js/contact.js
+++ js/contact.js
@@ -1,53 +1,50 @@
-
 function sendMessage() {
   var nameInput = document.querySelector('input[type="text"][name="name"]');
   var inputEmail = document.querySelector('input[type="email"][name="email"]');
   var phoneInput = document.getElementById('phone');
   var messageInput = document.querySelector('textarea[name="message"]');
-  var email = inputEmail.value;
+  var subjectInput = document.querySelector('input[type="text"][name="subject"]');
+
   var name = nameInput.value;
+  var email = inputEmail.value;
   var phone = phoneInput.value;
   var message = messageInput.value;
-  var chatId = '693372637';
+  var subject = subjectInput.value;
 
-  
+  fetch('config/telegram.php')
+      .then(response => response.json())
+      .then(data => {
+          var chatId = data.chatId;
+          var token = data.token;
+          var url = `https://api.telegram.org/bot${token}/sendMessage`;
 
-  var token = '6127064955:AAEn6_ogIks_bGQFN0MqQxNB9YVkfhK0T1g';
-  var text = `Name: ${name}%0A%0AEmail: ${email}%0A%0APhone number: ${phone}%0A%0ASubject: ${document.querySelector('input[type="text"][name="subject"]').value}%0A%0AMessage: ${message}`;
+          var text = `Name: ${name}\n\nEmail: ${email}\n\nPhone number: ${phone}\n\nSubject: ${subject}\n\nMessage: ${message}`;
 
-  var url = `https://api.telegram.org/bot${token}/sendMessage`;
-
-  var text = "Name: " + name + "\n\n" +
-           "Email: " + email + "\n\n" +
-           "Phone number: " + phone + "\n\n" +
-           "Subject: " + document.querySelector('input[type="text"][name="subject"]').value + "\n\n" +
-           "Message: " + message;
-
-  axios.post(url, {
-    chat_id: chatId,
-    text: text,
-  })
-    .then(function (response) {
-      swal({
-        title: "Success!",
-        text: "Your request has been sent!",
-        icon: "success",
-        button: "OK",
+          return axios.post(url, {
+              chat_id: chatId,
+              text: text,
+          });
+      })
+      .then(function (response) {
+          swal({
+              title: "Success!",
+              text: "Your request has been sent!",
+              icon: "success",
+              button: "OK",
+          });
+          nameInput.value = '';
+          inputEmail.value = '';
+          phoneInput.value = '';
+          messageInput.value = '';
+          subjectInput.value = '';
+      })
+      .catch(function (error) {
+          swal({
+              title: "Error",
+              text: "Failed to send message. Please try again later.",
+              icon: "error",
+              button: "OK",
+          });
+          console.log(error);
       });
-      nameInput.value = '';
-      inputEmail.value = '';
-      phoneInput.value = '';
-      messageInput.value = '';
-      document.querySelector('input[type="text"][name="subject"]').value = '';
-    })
-    .catch(function (error) {
-      swal({
-        title: "Error",
-        text: "Failed to send message. Please try again later.",
-        icon: "error",
-        button: "OK",
-      });
-      console.log(error);
-    });
 }
-
